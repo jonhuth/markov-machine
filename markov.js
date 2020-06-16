0c9f79a300782b124fc1695a1a324cd8f9046668
Jonathan Huth <jonhuth95@gmail.com>
2020-06-16 15:12:10 -0700

    finished MarkovMachin methods

--- markov.js
+++ markov.js
@@ -17,13 +17,44 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let markov_obj = {};
+    let words = this.words;
+    for(let i = 0; i < words.length; i++) {
+      let curWord = words[i];
+      let nextWord = words[i + 1] ? words[i + 1] : null;
+
+      if (curWord in markov_obj) {
+        markov_obj[curWord].push(nextWord);
+      }
+      else {
+        markov_obj[curWord] = [nextWord];
+      }
+    }
+    return markov_obj;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let chainsObj = this.makeChains();
+    let keysArr = Object.keys(chainsObj);
+    let randKey = keysArr[getRandomNum(keysArr.length)];
+    let randVals = chainsObj[randKey];
+    let randWord = randVals[getRandomNum(randVals.length)];
+
+    let randWords = [];
+
+    while (randWord !== null && randWords.length < numWords) {
+      randWords.push(randWord);
+      randVals = chainsObj[randWord];
+      randWord = randVals[getRandomNum(randVals.length)];
+    }
+    return randWords.join(' ');
   }
+}
+
+
+function getRandomNum(max) {
+  return Math.floor(Math.random() * max);
 }
